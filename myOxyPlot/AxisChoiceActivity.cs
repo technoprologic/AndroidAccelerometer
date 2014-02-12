e8de9176eaa4a4996d1873c850bd6e87046b2681
technoprologic <technoprologic@gmail.com>
2014-02-12 13:01:30 +0100

    fwerwer

--- myOxyPlot/AxisChoiceActivity.cs
+++ myOxyPlot/AxisChoiceActivity.cs
@@ -19,7 +19,7 @@ namespace myOxyPlot
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
-            //xcvvdfddsff
+            //xcvvdfddsffsdfdf
             // Create your application here
             SetContentView(Resource.Layout.AxisChoice);
 
